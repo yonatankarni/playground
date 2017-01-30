0135b186b3aa71e1f6b7cd89d0ccbfdbdeca63c0
Yonatan Karni <ykarni@outbrain.com>
2017-01-30 09:56:39 +0200

    solution for 2nd task

--- adventOfCode2016/day3/day3.scala
+++ adventOfCode2016/day3/day3.scala
@@ -1,14 +1,31 @@
 package adventofcode.day3
 
 object SquaresWithThreeSides extends App {
-    println(
-    	scala.io.Source.fromFile("input.txt")
-    	.getLines.map(line => line.split(' ')
-    		.map(_.trim)
-    		.filter(_.isEmpty == false)
-    		.map(_.toInt))
-    	.map(a => (a(0),a(1),a(2)))
+	println(
+		scala.io.Source.fromFile("input.txt").getLines
+			.map(line => line.split(' ')
+				.map(_.trim)
+				.filter(!_.isEmpty)
+				.map(_.toInt))
+			.map(a => (a(0),a(1),a(2)))
+			.filter(t => (t._1 + t._2 > t._3) && (t._1 + t._3 > t._2) && (t._2 + t._3 > t._1))
+			.size
+	)
+
+	val arr = scala.io.Source.fromFile("input.txt").getLines
+		.map(line => line.split(' ')
+			.map(_.trim)
+			.filter(!_.isEmpty)
+			.map(_.toInt))
+		.toArray
+
+	val triplets = for {
+		a <- 0 to arr.length - 3 by 3
+		b <- 0 to 2
+		} yield Seq(arr(a)(b), arr(a+1)(b), arr(a+2)(b))
+
+	val res = triplets.map(a => (a(0),a(1),a(2)))
 		.filter(t => (t._1 + t._2 > t._3) && (t._1 + t._3 > t._2) && (t._2 + t._3 > t._1))
-    	.size
-		)
+		.size
+	println(res)
 }
